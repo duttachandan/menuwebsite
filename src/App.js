76b66202aa8cc80ee3dbf19e248b94113bf3c0df
Chandan Dutta <duttaankit917@gmail.com>
2023-08-31 23:58:28 +0530

    new file:   src/list.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import list from "./list";
 
 function App() {
+  function dragme(e) {
+    const cursor = document.querySelector(".cursor");
+    cursor.style.left=e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-div flex' onMouseMove={(e)=>dragme(e)}>
+        {list.map((e, index) => {
+          return (
+            <div className="cards" key={index}>
+              <div className='cards-img'>
+                <img src={e.img} alt="" />
+              </div>
+              <div className='content'>
+                <h2 className='content-header'>{e.header}</h2>
+                <p className='content-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa natus esse recusandae dolores asperiores ullam beatae perspiciatis iusto eos iure!</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className='cursor'></div>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/list.js
@@ -0,0 +1,16 @@
+const list = [
+    {
+        img:"https://images.unsplash.com/photo-1687360440886-f220f137a16c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        header:"Main App"
+    },
+    {
+        img:"https://images.unsplash.com/photo-1687360440781-93a491d8eb58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+        header:"HIki App"
+    },
+    {
+        img:"https://images.unsplash.com/photo-1680833929488-575890a7f8b5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+        header:"MohammadReza BaBaei"
+    }
+]
+
+export default list;
